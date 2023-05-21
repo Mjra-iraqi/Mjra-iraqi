8608b10a8834f9a83652b0907bd5ae5a87d762ff
Almohsen <50508250+ALLMOHSEN@users.noreply.github.com>
2023-05-21 11:59:49 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,20 +1,31 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// counter.js
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCdyutD4Kxvk8NZOxzvw2D00GWdBkfjDdQ",
-    authDomain: "mjrairaqi-4d215.firebaseapp.com",
-    databaseURL: "https://mjrairaqi-4d215-default-rtdb.firebaseio.com",
-    projectId: "mjrairaqi-4d215",
-    storageBucket: "mjrairaqi-4d215.appspot.com",
-    messagingSenderId: "463411067509",
-    appId: "1:463411067509:web:b2a206516547635286563c"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase, ref, transaction } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const firebaseConfig = {
+  apiKey: "AIzaSyCdyutD4Kxvk8NZOxzvw2D00GWdBkfjDdQ",
+  authDomain: "mjrairaqi-4d215.firebaseapp.com",
+  databaseURL: "https://mjrairaqi-4d215-default-rtdb.firebaseio.com",
+  projectId: "mjrairaqi-4d215",
+  storageBucket: "mjrairaqi-4d215.appspot.com",
+  messagingSenderId: "463411067509",
+  appId: "1:463411067509:web:b2a206516547635286563c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase();
+
+// Increase the visit counter on page load
+const incrementPageVisits = () => {
+  const pageVisitsRef = ref(database, 'pageVisits');
+  transaction(pageVisitsRef, (currentVisits) => {
+    if (currentVisits === null) {
+      return 1;
+    }
+    return currentVisits + 1;
+  });
+};
+
+window.addEventListener('DOMContentLoaded', incrementPageVisits);

a4ea0800378589657bc6e724d1a2a76e47a0c829
Almohsen <50508250+ALLMOHSEN@users.noreply.github.com>
2023-05-21 10:15:30 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,31 +1,31 @@
 // عدد الزيارات المبدئي
-
 let counter = 0;
 
 // زيادة العداد وحفظه
-
 function increaseCounter() {
-
   counter++;
-
-  // قم بإرسال العداد إلى الملف في الاستضافة للحفظ
-
+  // استدعاء ملف PHP لزيادة العداد
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "counter.php");
+  xhr.send();
+  
   // قم بتحديث عنصر العرض في الصفحة
-
   document.getElementById("counter").textContent = counter;
-
 }
 
 // تحميل العداد عند تحميل الصفحة
-
 window.addEventListener("load", function() {
-
-  // قم بتحميل العداد من الملف في الاستضافة
-
-  // قم بتحديث عنصر العرض في الصفحة
-
-  document.getElementById("counter").textContent = counter;
-
+  // استدعاء ملف PHP لتحميل العداد
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        counter = parseInt(xhr.responseText);
+        // قم بتحديث عنصر العرض في الصفحة
+        document.getElementById("counter").textContent = counter;
+      }
+    }
+  };
+  xhr.open("GET", "counter.php");
+  xhr.send();
 });
-
- 

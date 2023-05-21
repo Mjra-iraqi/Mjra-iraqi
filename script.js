ef0bce67199434125e8a40e5aecea11088c606ea
Almohsen <50508250+ALLMOHSEN@users.noreply.github.com>
2023-05-21 08:56:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-// استرجاع قيمة عدد الزيارات من التخزين المحلي إذا كانت متاحة
-var visitCount = localStorage.getItem('visitCount');
+// استيراد مكتبة Firebase
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-// التحقق من وجود قيمة عدد الزيارات في التخزين المحلي
-if (visitCount) {
-    visitCount = parseInt(visitCount); // تحويل القيمة إلى رقم صحيح
-} else {
-    visitCount = 0; // قيمة افتراضية للزيارات إذا لم يتم العثور على قيمة في التخزين المحلي
-}
+// تكوين اتصال Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyCdyutD4Kxvk8NZOxzvw2D00GWdBkfjDdQ",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "mjrairaqi-4d215",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+};
 
-// زيادة قيمة عدد الزيارات بمقدار واحد
-visitCount++;
+// تهيئة التطبيق Firebase
+firebase.initializeApp(firebaseConfig);
 
-// تحديث العنصر المعرف بـ visitCount في صفحة HTML
-document.getElementById('visitCount').textContent = visitCount;
+// الحصول على مرجع قاعدة البيانات
+var database = firebase.database();
 
-// تخزين قيمة عدد الزيارات المحدثة في التخزين المحلي
-localStorage.setItem('visitCount', visitCount.toString());
+// استرجاع عدد الزيارات من قاعدة البيانات وتحديثها
+database.ref('/visitCount').once('value', function(snapshot) {
+    var visitCount = snapshot.val();
+    if (visitCount) {
+        visitCount = parseInt(visitCount);
+    } else {
+        visitCount = 0;
+    }
+
+    visitCount++;
+
+    // تحديث قيمة عدد الزيارات في قاعدة البيانات
+    database.ref('/visitCount').set(visitCount);
+
+    // تحديث العنصر المعرف بـ visitCount في صفحة HTML
+    document.getElementById('visitCount').textContent = visitCount;
+});
